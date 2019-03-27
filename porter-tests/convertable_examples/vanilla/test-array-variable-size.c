642112527213bad8f26c763c7310eca56f85f477
srajg <shrirajgandhi@gmail.com>
2019-03-27 10:37:42 -0400

    add new examples

---
+++ porter-tests/convertable_examples/vanilla/test-array-variable-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void make_array(int len) {
+	int[] arr = new arr[len];
+
+	for (int i = 0; i < len; ++i) {
+		arr[i] = i;
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	for (int i = 0; i < 100; ++i) {
+		make_array(i);
+	}
+
+
+	return 0;
+}
---
+++ porter-tests/convertable_examples/vanilla/test-len-ptr.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int garbage_sum(int *arr, int *len) 
+{
+	int sum = 0;
+
+	for (int i = 0; i < *len; i++)
+		sum += arr[i];
+
+	return sum;
+}
+
+int main(int argc, char const *argv[])
+{
+	int *len = malloc(sizeof(int));
+	*len = 10;
+
+	int *arr = malloc((*len) * sizeof(int));
+
+	garbage_sum(arr, len);
+	
+	return 0;
+}
